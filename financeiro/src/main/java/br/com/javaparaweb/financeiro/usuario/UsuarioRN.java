bd1fd2aabe9c796edb2f56119ad440b9e2a6e6ae
felipegabriel1942 <pinheiro_felipeg@yahoo.com.br>
2018-06-07 12:34:18 -0300

    Criação da página de cadastro de usuarios

---
+++ financeiro/src/main/java/br/com/javaparaweb/financeiro/usuario/UsuarioRN.java
@@ -0,0 +1,47 @@
+package br.com.javaparaweb.financeiro.usuario;
+
+import java.util.List;
+/**
+ * Essa classe trabalha auxiliando para a interface(telas)
+ * @author Felipe
+ *
+ */
+public class UsuarioRN {
+	
+	private UsuarioDAO usuarioDAO;
+	
+	public UsuarioRN() {
+		this.usuarioDAO = DAOFactory.criarUsuarioDAO();
+	}
+	
+	//Carrega uma unica instância com base emm seu código
+	public Usuario carregar(Integer codigo) {
+		return this.usuarioDAO.carregar(codigo);
+	}
+	
+	//Carrega as informações do usuaário logo após o login
+	public Usuario buscarPorLogin(String login) {
+		return this.usuarioDAO.buscarPorLogin(login);
+	}
+	
+	//Salva ou atualiza um usuario
+	public void salvar(Usuario usuario) {
+		Integer codigo = usuario.getCodigo();
+		
+		//Verifica se o usuario ja existe ou não
+		//Se não existir cria o usuario se existe faz o update
+		if(codigo == null || codigo == 0) {
+			this.usuarioDAO.salvar(usuario);
+		} else {
+			this.usuarioDAO.atualizar(usuario);
+		}
+	}
+	
+	public void excluir(Usuario usuario) {
+		this.usuarioDAO.excluir(usuario);
+	}
+	
+	public List<Usuario> listar(){
+		return this.usuarioDAO.listar();
+	}
+}
